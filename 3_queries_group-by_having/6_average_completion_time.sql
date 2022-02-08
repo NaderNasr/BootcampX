025cc747cc1325f261e913dde40f3a625ec4d38b
Nader Nasr <nadernasr7@gmail.com>
2022-02-08 13:46:17 -0500

    code clean up

--- 3_queries_group-by_having/6_average_completion_time.sql
+++ 3_queries_group-by_having/6_average_completion_time.sql
@@ -1,9 +1,4 @@
--- Get currently enrolled students' average assignment completion time.
-    -- Select the students name and average time from assignment submissions.
-    -- Order the results from greatest duration to least greatest duration.
-    -- A student will have a null end_date if they are currently enrolled
-
-SELECT students.name as student, AVG(assignment_submissions.duration) as average_assignment_duration  
+SELECT students.name as student, ROUND(AVG(assignment_submissions.duration)) as average_assignment_duration  
 FROM students
 JOIN assignment_submissions ON student_id = students.id
 GROUP BY students.name, students.end_date
--- 3_queries_group-by_having/7_low_average_completion_time.sql
+++ 3_queries_group-by_having/7_low_average_completion_time.sql
@@ -1,11 +1,3 @@
-
--- Get the students who's average time it takes to complete an assignment is less than the average estimated time it takes to complete an assignment.
-
---     Select the name of the student, their average completion time, and the average suggested completion time.
---     Order by average completion time from smallest to largest.
---     Only get currently enrolled students.
---     This will require data from students, assignment_submissions, and assignments.
-
 SELECT students.name as student, AVG(assignment_submissions.duration) as average_assignment_duration, AVG(assignments.duration) as average_estimated_duration  
 FROM students
 JOIN assignment_submissions ON student_id = students.id
@@ -14,3 +6,4 @@ WHERE end_date IS NULL
 GROUP BY students.name, students.end_date
 HAVING avg(assignment_submissions.duration) < avg(assignments.duration)
 ORDER BY AVG(assignment_submissions.duration);
+
